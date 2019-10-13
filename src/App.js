fa25e75cc4cbeb87a04a7b1e181ed352106b1c76
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-10-14 00:07:41 +0800

    fix crash build on android

--- src/App.js
+++ src/App.js
@@ -2,34 +2,22 @@ import React from 'react';
 import { Keyboard, Platform, SafeAreaView, StyleSheet } from 'react-native';
 import Main from 'screens/main';
 import * as deviceActions from 'ducks/device';
-import Geolocation from 'react-native-geolocation-service';
+import Geolocation from '@react-native-community/geolocation';
 
 class App extends React.PureComponent {
   componentDidMount() {
-    Keyboard.addListener(
-      Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow',
-      this.onKeyboardAppear
-    );
-    Keyboard.addListener(
-      Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide',
-      this.onKeyboardHide
-    );
+    Keyboard.addListener(Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow', this.onKeyboardAppear);
+    Keyboard.addListener(Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide', this.onKeyboardHide);
 
     Geolocation.getCurrentPosition(
       (position) => deviceActions.setGPS({ latitude: position.coords.latitude, longitude: position.coords.longitude }),
-      (error) => {
-        // See error code charts below.
-        console.log(error.code, error.message);
-      },
+      (error) => console.log(error.code, error.message),
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     );
   }
 
   onKeyboardAppear = e =>
-    deviceActions.setKeyboard({
-      isKeyboardAppear: true,
-      keyboardHeight: e.endCoordinates.height,
-    });
+    deviceActions.setKeyboard({ isKeyboardAppear: true, keyboardHeight: e.endCoordinates.height });
 
   onKeyboardHide = () =>
     deviceActions.setKeyboard({ isKeyboardAppear: false, keyboardHeight: 0 });
